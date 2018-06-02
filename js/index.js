b5d42afa084af8c0b0884bbbc2b6583eca7d851b
Simonzzzzmmmm <11510695@mail.sustc.edu.cn>
2018-06-02 23:22:58 +0800

    change on make trans panel

--- js/index.js
+++ js/index.js
@@ -16,4 +16,91 @@ if(document.location.pathname != "/pen/secure_iframe") {
        i++;
   }, 250);
 }
+$('.form-head').click(function(){
 
+    if($(this).closest('.grop-from').attr('id')=='signup'){
+        $('.grop-from').attr('id' , 'name');
+        $('.icon-action').addClass('back');
+    }
+    else if($(this).closest('.grop-from').attr('id')=='success'){
+        $('.grop-from').attr('id' , 'signup');
+        $('input').val('');
+    }
+
+});
+
+
+
+
+$('.form-action').click(function(){
+
+    var form_id = $('.grop-from').attr('id');
+    $('.icon-action').addClass('back');
+
+    if($('#control-' + form_id).val() != ''){
+        switch (form_id) {
+            case 'name':
+                form_id = "value";
+                break;
+            case "value":
+                form_id = "account";
+                break;
+            case "account":
+                form_id = "password";
+                break;
+            case "password":
+                form_id = "password-repeat";
+                break;
+            case "password-repeat":
+                form_id = "success";
+                break;
+            case "success":
+                window.location.href = "DashBoard2.html";
+                break;
+        }
+        $('.icon-action').addClass('back');
+
+    }
+
+    else{
+        switch (form_id) {
+            case 'name':
+                form_id = "signup";
+                $('.icon-action').removeClass('back');
+                break;
+            case "value":
+                form_id = "name";
+                break;
+            case "account":
+                form_id = "value";
+                break;
+            case "password":
+                form_id = "account";
+                break;
+            case "password-repeat":
+                form_id = "password";
+                break;
+             case "success":
+                 window.location.href = "DashBoard2.html";
+                break;
+        }
+        $('.icon-action').removeClass('back');
+    }
+
+    $('.grop-from').attr('id' , form_id);
+
+});
+
+$('input').keyup(function(){
+    $('.grop-from').removeClass('error');
+    $('.error-text').fadeOut();
+
+    if($(this).val()!=''){
+        $('.icon-action').removeClass('back');
+    }
+    else{
+        $('.icon-action').addClass('back');
+    }
+
+
+})
